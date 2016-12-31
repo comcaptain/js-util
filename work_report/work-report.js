3ae2f680a8a8cd6fdb5f1b1260a5572a5c10c4c0
孙国强 <master@pygmalion.click>
2016-12-31 10:52:50 +0900

    update to fit new page inside frame

--- work_report/work-report.js
+++ work_report/work-report.js
@@ -1,4 +1,5 @@
-var trs = [].slice.call(document.querySelectorAll("#KnmTbl tr.dtTR"));
+var doc = document.querySelector("[name=WM_FUNC]").contentDocument;
+var trs = [].slice.call(doc.querySelectorAll("#KnmTbl tr.dtTR"));
 trs.filter(tr => {
 	return tr.querySelector("select[name=sltKnmKt]").value == "1 1 100420 102139";
 }).forEach(tr => {
