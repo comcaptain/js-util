8402d8fead958d96dba841f5ca49d51fa1670465
Tony <htmlcaptain@gmail.com>
2022-08-12 01:05:29 +0900

    Download one chapter's content

--- typescript-util/src/novelDownloader/download.ts
+++ typescript-util/src/novelDownloader/download.ts
@@ -1,16 +1,52 @@
 import fetch from "node-fetch";
 import parse from "node-html-parser";
 
+interface Chapter
+{
+	name: string;
+	url: string;
+	content?: string;
+}
+
 export default async function download()
 {
-	const response = await fetch("https://www.exambook.net/book/28282.html");
-	const binary = await response.arrayBuffer();
-	const decoder = new TextDecoder("GBK");
-	const html = decoder.decode(binary);
-	const doc = parse(html);
-	const chapterLinks = doc.querySelectorAll("#list-chapterAll > dl > dd > a");
-	chapterLinks.forEach(a =>
+	const hostName = "https://www.exambook.net";
+	const chapters = (await downloadDoc(`${hostName}/book/28282.html`))
+		.querySelectorAll("#list-chapterAll > dl > dd > a")
+		.map(link =>
+		{
+			const chapter: Chapter = {
+				name: link.textContent.trim(),
+				url: `${hostName}${link.getAttribute("href")}`
+			}
+			return chapter;
+		})
+	console.log(await downloadChapter(chapters[0]));
+}
+
+async function downloadChapter(chapter: Chapter): Promise<Chapter>
+{
+	let doc = await downloadDoc(chapter.url);
+	let content = "";
+	while (true)
 	{
-		console.log(`${a.textContent} ${a.getAttribute("href")}`)
-	})
+		let currentContent = doc.querySelector("#htmlContent")?.textContent.trim();
+		content += currentContent;
+		const nextLink = doc.querySelector("#linkNext");
+		if (!nextLink || nextLink.textContent.trim() !== "下一页") break;
+		doc = await downloadDoc(nextLink.getAttribute("href")!.trim())
+	}
+	return {
+		...chapter,
+		content
+	};
+}
+
+async function downloadDoc(url: string, encoding: string = "GBK")
+{
+	const response = await fetch(url);
+	const binary = await response.arrayBuffer();
+	const decoder = new TextDecoder(encoding);
+	const html = decoder.decode(binary);
+	return parse(html);
 }
